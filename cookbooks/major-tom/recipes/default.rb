e884d921c9efb0ae2a5e8fa1585e99195187cc26
Krishna Srihari <krishna.srihari@gmail.com>
2012-11-26 12:30:34 +0530

    updates

--- cookbooks/major-tom/recipes/default.rb
+++ cookbooks/major-tom/recipes/default.rb
@@ -6,3 +6,15 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+
+remote_file "#{Chef::Config[:file_cache_path]}/background.png" do
+	source 'http://placehold.it/1440x900'
+	notifies :run, 'execute[set Desktop background]', :immediately
+end
+
+execute 'set Desktop background' do
+	command "gconftool-2 -t string -s /desktop/gnome/background/picture_filename #{Chef::Config[:file_cache_path]}/background.png"
+	user 'krishna'
+	action :nothing
+end
