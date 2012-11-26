5cb098944d5d626d9f852c4117dea4b5dbc85af3
Krishna Srihari <krishna.srihari@gmail.com>
2012-11-26 11:11:36 +0530

    metadata

--- cookbooks/major-tom/metadata.rb
+++ cookbooks/major-tom/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Krishna Srihari"
 maintainer_email "krishna@skilledtree.com"
 license          "All rights reserved"
-description      "Installs/Configures major-tom"
+description      "Installs/Configures desktop background images"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.0.1"
