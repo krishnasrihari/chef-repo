359669dc395d4878aa4ee84548548241568f4760
krishnasrihari <krishna.srihari@gmail.com>
2012-11-28 06:28:48 -0500

    redis cookbook init

---
+++ cookbooks/redis/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "SkilledTree"
+maintainer_email "krishna.srihari@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures redis"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
